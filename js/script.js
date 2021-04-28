6424fe1867e63c5419b68d2c74ac490bb8dea06d
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-04-28 17:12:20 +0200

    Stampo le option della select

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+// Attraverso una chiamata ajax all’API di boolean avremo a disposizione una decina di dischi musicali. Stampiamo a schermo una card per ogni album.
+// BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2: Ordinare i dischi per anno di uscita.
+
 var app = new Vue (
     {
         el: '#root',
         data: {
-            albumsList: []
+            // Array che viene popolato dalla chiamata ajax all'API di Boolean
+            albumsList: [],
+
+            // Arrau che viene popolato dai generi musicali
+            albumsGenre: [],
+
+            // Selected option
+            selected: ''
         },
         methods: {
 
@@ -13,6 +24,15 @@ var app = new Vue (
                 .then( (response) => {
                     const result = response.data;
                     this.albumsList = result.response;
+
+                    // Itero l'array di album e pusho in un array il genere di ogni album solamente se non è già presente
+                    this.albumsList.forEach(element => {
+                        if ( !this.albumsGenre.includes(element.genre) ) {
+                            this.albumsGenre.push(element.genre);
+                        }
+                    });
+
+                    
                 });
         }
     }
