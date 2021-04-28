9318f2c3dee6e7e3bd0474f2759360f40ff37ef6
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-04-28 16:00:26 +0200

    stampo ogni album

--- js/script.js
+++ js/script.js
@@ -2,10 +2,18 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-            
+            albumsList: []
         },
         methods: {
 
         },
+        mounted() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/array/music ')
+                .then( (response) => {
+                    const result = response.data;
+                    this.albumsList = result.response;
+                });
+        }
     }
 );
